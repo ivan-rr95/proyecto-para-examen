7c2d0c9640177fa20a45c99a00e5dbe99aa3bd22
ivan-rr95 <ivan.rodriguez.rodriguez.1995@gmail.com>
2022-07-13 11:30:11 +0200

    gh-pages commit

---
+++ js/about.51dc98c8.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkproyecto_para_examen"]=self["webpackChunkproyecto_para_examen"]||[]).push([[443],{247:function(a,e,n){n.r(e),n.d(e,{default:function(){return f}});var r=n(252);const t={class:"about"},u=(0,r._)("h1",null,"This is an about page",-1),c=[u];function o(a,e){return(0,r.wg)(),(0,r.iD)("div",t,c)}var s=n(744);const i={},p=(0,s.Z)(i,[["render",o]]);var f=p}}]);
+//# sourceMappingURL=about.51dc98c8.js.map
